95ab22ebb3e003fda369ba66d879f1afca92cdef
João <jv.dossantos012@gmail.com>
2023-07-17 23:14:15 -0300

    projeto ignite-shop

---
+++ src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
+import Image from "next/image"
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from "../styles/pages/app"
+
+
+globalStyles()
+
+export function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
+}
+
+export default MyApp
